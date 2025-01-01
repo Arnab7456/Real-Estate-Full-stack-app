3a94ffbd49f6fafdb8070f3ebcfc383229379268
Arnab Das <93949960+Arnab7456@users.noreply.github.com>
2025-01-02 04:42:54 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
-import startKeepAlivePoll from './keepAlive.js'; 
+ 
 
 dotenv.config();
 
@@ -26,7 +26,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Start keep-alive polling
-startKeepAlivePoll();
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000!');
